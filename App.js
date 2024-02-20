5fb2af160dfe39a1b20d20a50ee991ee4fdee3aa
HP <HP@DESKTOP-DITENS6>
2024-02-21 00:22:18 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+const heading=React.createElement("div",
+{id:"parent"},
+[
+    React.createElement("div",{id:"child1"},
+    [
+        React.createElement("h1",{id:"heading"},"Hi from first"),
+        React.createElement("h2",{id:"heading2"},"Hi from second")
+    ]
+    ),
+    React.createElement("div",{id:"child2"},
+    [
+        React.createElement("h1",{id:"heading"},"Hi from first"),
+        React.createElement("h2",{id:"heading2"},"Hi from second")
+    ]
+    )
+]);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
