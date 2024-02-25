26ba354b0515aaf8fd141343189814132f5678a8
HP <HP@DESKTOP-DITENS6>
2024-02-25 12:55:31 +0530

    ignite

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
 const heading=React.createElement("div",
 {id:"parent"},
 [
     React.createElement("div",{id:"child1"},
     [
-        React.createElement("h1",{id:"heading"},"Hi from first"),
+        React.createElement("h1",{id:"heading"},"Hi from parcel"),
         React.createElement("h2",{id:"heading2"},"Hi from second")
     ]
     ),
